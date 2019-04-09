78196ef21e2c122b9768f32e72f4b60578990334
Yana Agun Siswanto <yana.developer@gmail.com>
2019-04-10 06:03:18 +0700

    Initial Version

---
+++ background.js
@@ -0,0 +1,20 @@
+async function openSciHubPdf() {
+  browser.storage.local.get("currentUrl", function (storage) {
+    browser.tabs.create({
+      url: "https://sci-hub.tw/" + storage.currentUrl,
+    });
+  })
+}
+
+async function updateCurrentPage(details) {
+  return browser.storage.local.set({
+    currentUrl: details.url,
+  })
+}
+
+browser.webRequest.onBeforeRequest.addListener(updateCurrentPage, {
+  urls: ["<all_urls>"]
+});
+
+
+browser.browserAction.onClicked.addListener(openSciHubPdf);
