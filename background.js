80611635902031de60e3b027461edf4f322c1a4e
Yana Agun Siswanto <yana.developer@gmail.com>
2019-10-25 04:00:12 +0700

    Use tabs api instead

--- background.js
+++ background.js
@@ -1,17 +1,24 @@
 const SCIHUB_URL = 'scihub_url';
-const DEFAULT_SCIHUB_URL = 'https://sci-hub.tw/';
+const DEFAULT_SCIHUB_URL = 'https://sci-hub.tw';
 const CURRENT_URL = "current_url";
 let storage = browser.storage.sync;
 let config = {};
 
 config[SCIHUB_URL] = DEFAULT_SCIHUB_URL;
-config[CURRENT_URL] = null;
 
 function openSciHubPdf() {
-  storage.get(config, function (config) {
-    browser.tabs.create({
-      url: config[SCIHUB_URL] + '/' + config[CURRENT_URL],
-    });
+  // Get current path
+  browser.tabs.query({currentWindow: true, active: true})
+    .then((tabs) => {
+      if(tabs.length > 0) {
+        // Get the CURRENT_URL configuration
+        storage.get(config, function (config) {
+          current_active_url = tabs[0].url
+          browser.tabs.create({
+            url: config[SCIHUB_URL] + '/' + current_active_url,
+          });
+        })
+      }
   })
 }
 
@@ -20,11 +27,5 @@ function updateCurrentPage(details) {
   config[CURRENT_URL] = details.url;
   return storage.set(config);
 }
-
-browser.webRequest.onBeforeRequest.addListener(updateCurrentPage, {
-  urls: ["<all_urls>"],
-  types: ["main_frame"]
-});
-
 
 browser.browserAction.onClicked.addListener(openSciHubPdf);
